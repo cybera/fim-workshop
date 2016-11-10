ff50d0490ad8f2324c43b6a0e6a852e5690d9ab9
Andrew Klaus <andrewklaus@gmail.com>
2016-11-10 01:52:41 +0000

    no longer setting up openconext hub for workshop

--- openconext-deploy/PROVISION_DEV.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-ansible-playbook -v \
-    -i "environments/workshop/inventory" \
-    -u "centos" \
-    --extra-vars="secrets_file=environments/workshop/secrets/workshop.yml" \
-    -K provision-vm.yml \
